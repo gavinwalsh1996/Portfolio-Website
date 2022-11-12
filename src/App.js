28977c4e72b4a49cc818f899943b0e98f11bb42a
Gavin <gavinwalsh739@gmail.com>
2022-11-12 16:42:53 +0100

    project added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ import Footer from './components/Footer'
 import Contact from './pages/Contact';
 
 //Images
-import Portfolio from './images/fes screenshot.png'
+import FesImage from './images/fes screenshot.png'
+import PortfolioImage from './images/portfolio-website-screenshot.png'
 
 //Test
 export const ThemeContext = React.createContext(null);
@@ -28,18 +29,20 @@ function App() {
 
   let heading = 'Projects';
 
-  let portfolioProject = [
+  let projects = [ 
     {
-      title: 'Portfolio Website', 
-      description: 'My portfolio website',
-      img: Portfolio,
-      gitHub: 'https://github.com/gavinwalsh1996/flight-entertainment-system'
-    },
+      title: 'Flight Entertainment System', 
+      description: 'Experience a flight from Dublin to Barcelona with this React Front end flight entertainment system app',
+      img: FesImage,
+      gitHub: 'https://github.com/gavinwalsh1996/flight-entertainment-system',
+      LiveApp: 'https://bucolic-dango-c7b9a7.netlify.app'
+    }, 
     {
-      title: 'Portfolio Website',
-      description: 'My portfolio website',
-      img: Portfolio,
-      gitHub: 'https://github.com/gavinwalsh1996/flight-entertainment-system'
+      portTitle: 'Portfolio Website',
+      portDescription: 'My portfolio website',
+      portImg: PortfolioImage,
+      portGitHub: 'https://github.com/gavinwalsh1996/Portfolio-Website/tree/master',
+      portLiveApp: 'https://heroic-gumdrop-f6e4a5.netlify.app'
     }
   ]
   
@@ -50,8 +53,8 @@ function App() {
         <Home />
         <About />
         <Projects heading={heading}/>
-        <Projects projectss={portfolioProject}/>
-        <Projects />
+        {/* <Projects projects={projects}/> */}
+        {/* <Projects /> */}
         <Contact />
         <Footer />
     </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,21 +3,36 @@ import Heading from '../components/Heading'
 import SmlButton from '../components/SmlButton'
 import '../css/Projects.scss'
 import FES from '../images/fes screenshot.png'
+// import PortfolioImg from '../images/portfolio-website-screenshot.png'
+import PortfolioImg2 from '../images/portfolio-website.png'
 
 
-function Projects({heading}) {
+function Projects({heading, projects}) {
+
 
 
   let buttonValue1= 'Live App';
   let buttonValue2= 'Learn More';
   let FlightEntLink = 'https://github.com/gavinwalsh1996/flight-entertainment-system';
+  let FlightEntLinkWebsite = 'https://bucolic-dango-c7b9a7.netlify.app';
+  let PortfolioLink = 'https://github.com/gavinwalsh1996/Portfolio-Website/tree/master'
+  let PortfolioLinkWebsite = 'https://heroic-gumdrop-f6e4a5.netlify.app'
 
 
   return (
+    <>
+
+    {/* {projects.map((project) => (
+      <>
+        <h1>{project[0].title}</h1>
+      
+      </>
+    ))}  */}
 
   <div className='projects'>
       
   <Heading heading={heading} />
+  
 
   <div className='projects-container'>
     <div className="project-image">
@@ -29,8 +44,25 @@ function Projects({heading}) {
           React Front end flight entertainment system app
         </p>
         <div className="buttons">
-        <SmlButton value={buttonValue1} />
+        <SmlButton value={buttonValue1} link={FlightEntLinkWebsite} />
         <SmlButton value={buttonValue2} link={FlightEntLink} />
+        </div>
+      </div> 
+
+  </div>
+
+  {/* Duplicate for now for test. Recreate with props. */}
+
+  <div className='projects-container'>
+    <div className="project-image">
+        <img src={PortfolioImg2} alt='' />
+    </div>
+    <div className="project-desc">
+        <h3>Portfolio Website</h3>
+        <p>My portfolio website built with React and styled with SASS</p>
+        <div className="buttons">
+        <SmlButton value={buttonValue1} link={PortfolioLinkWebsite} />
+        <SmlButton value={buttonValue2} link={PortfolioLink} />
         </div>
       </div> 
 
@@ -38,7 +70,7 @@ function Projects({heading}) {
 
   </div>
   
-
+  </>
   )
 }
 
